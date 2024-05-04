bdb4d0eaa02da2d9f8f62de0ff23db0b6dc37c9a
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-05-04 23:09:53 +0300

    Lesson 05 added

---
+++ lesson05/src/test/java/ru/gb/lesson05/Lesson05ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.lesson05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
