355e819e877e33fbec01b998ab9d6ac5eae1c5f8
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-05-08 00:45:24 +0300

    Lesson 06 exercise02 bugfix

--- lesson06/exercise02/src/main/java/ru/gb/Exercise02Application.java
+++ lesson06/exercise02/src/main/java/ru/gb/Exercise02Application.java
@@ -3,15 +3,6 @@ package ru.gb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Структура заметки:
-//		- ID (автоинкрементное)(тип - Long)
-//		- Заголовок (не может быть пустым)(тип - String)
-//		- Содержимое (не может быть пустым)(тип - String)
-//		- Дата создания (автоматически устанавливается при создании заметки)(тип - LocalDateTime)
-//
-//Подсказка:
-//Репозиторий насладует JpaRepository<Note, Long>. В репозитории добавляем метод Optional<Note> findById(Long id);
-
 @SpringBootApplication
 public class Exercise02Application {
 
