75d2fca9994608e744036ea50ee826bfc9f813a7
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-05-18 21:28:21 +0300

    Lesson 10 api layout added

---
+++ lesson10/testing-exercise/src/main/java/ru/gb/domain/Customer.java
@@ -0,0 +1,26 @@
+package ru.gb.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Customer(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
