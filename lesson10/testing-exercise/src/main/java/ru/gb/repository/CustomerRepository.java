f407d58721a350b5b94ab3981330a88e3f8c349b
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-05-19 16:26:15 +0300

    Lesson 10 re init

--- lesson10/testing-exercise/src/main/java/ru/gb/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.gb.domain.Customer;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
