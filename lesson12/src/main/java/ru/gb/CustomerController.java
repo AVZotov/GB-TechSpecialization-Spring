aaa0e816f369801b0c00385124f7b518095a739e
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-05-25 12:49:49 +0300

    Lesson12 init commit with base project layout

---
+++ lesson12/src/main/java/ru/gb/CustomerController.java
@@ -0,0 +1,47 @@
+package ru.gb;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.domain.Customer;
+import ru.gb.service.CustomerService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customers")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @GetMapping("/get_all")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return new ResponseEntity<>(customerService.getAllCustomers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> findCustomerById(@PathVariable Long id) {
+        return new ResponseEntity<>(customerService.findCustomerById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/add_new")
+    public ResponseEntity<?> addNewCustomer(@RequestBody Customer customer) {
+        customerService.addCustomer(customer);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateCustomer(@RequestBody Customer customer) {
+        customerService.updateCustomer(customer);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCustomerById(@PathVariable Long id) {
+        customerService.deleteCustomerById(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
